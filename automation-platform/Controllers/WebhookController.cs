87ea7acec5c401028c57176f8175ff6627cec866
p.rusnarczyk <p.rusnarczyk07@gmail.com>
2026-06-27 20:58:37 +0200

    Add webhook endpoint with logging

---
+++ automation-platform/Controllers/WebhookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using automation_platform.Dtos;
+
+namespace automation_platform.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebhookController : ControllerBase
+    {
+        private readonly ILogger<WebhookController> logger;
+        public WebhookController(ILogger<WebhookController> logger) 
+        { 
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWebhook(WebhookDto dto)
+        {
+            logger.LogInformation($"Webhook: \n Id: {dto.Id},\n Name: {dto.Name},\n Description: {dto.Description}");
+
+            return Ok(dto);
+        }
+    }
+}
